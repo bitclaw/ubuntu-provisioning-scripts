e05ac6c1a90dbc7c335dbcff3e28037fd64fe97c
Daniel Chavez <bitclaw@gmail.com>
2019-05-26 17:43:11 -0500

    Fix typo and and git cheat sheet, also remove gedit editor

--- install-geany.sh
+++ install-geany.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt remove gedit -y
 git clone https://github.com/codebrainz/geany-themes.git ~/code/github/public/geany-themes
 cd ~/code/github/public/geany-themes
 bash install.sh
