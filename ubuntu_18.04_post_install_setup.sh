4974ef32fe9c73efef631df7db4efe3c86c89fe7
Daniel Chavez <bitclaw@gmail.com>
2019-06-23 21:22:52 -0500

    Install gitflow

--- ubuntu_18.04_post_install_setup.sh
+++ ubuntu_18.04_post_install_setup.sh
@@ -186,3 +186,6 @@ sudo service procps start
 
 # Install NFS server, used for vagrant shared folder sometimes
 sudo apt install nfs-kernel-server -y
+
+# Install git flow
+sudo apt install git-flow
