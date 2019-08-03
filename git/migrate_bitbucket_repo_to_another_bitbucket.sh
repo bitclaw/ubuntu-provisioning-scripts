5b986b5e5cc74884fdd7c65f772ded666e0fcaeb
Daniel Chavez <bitclaw@gmail.com>
2019-08-03 10:40:58 -0500

    Add git migration scripts

---
+++ git/migrate_bitbucket_repo_to_another_bitbucket.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+BITBUCKET_USER=user1
+REPO_NAME=test1
+
+NEW_BITBUCKET_USER=user2
+NEW_REPO_NAME=test2
+
+git clone --mirror git@bitbucket.org:${BITBUCKET_USER}/${REPO_NAME}.git
+git remote add bitbucket git@bitbucket.org:${NEW_BITBUCKET_USER}/${NEW_REPO_NAME}.git
+git push bitbucket --mirror
--- git/migrate_github_repo_to_bitbucket.sh
+++ git/migrate_github_repo_to_bitbucket.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-git clone --mirror git@github.com:user/name-of-repo.git
-git remote add bitbucket git@bitbucket.org:name-of-repo/name-of-repo.git
+GITHUB_USER=user1
+GITHUB_REPO_NAME=test1
+
+BITBUCKET_USER=user2
+BITBUCKET_REPO_NAME=test2
+
+git clone --mirror git@github.com:${GITHUB_USER}/${GITHUB_REPO_NAME}.git
+git remote add bitbucket git@bitbucket.org:${BITBUCKET_USER}/${NEW_REPO_NAME}.git
 git push bitbucket --mirror
