8974b66a4da66b3a1dc3f2882d775163764cdfcd
Daniel Chavez <bitclaw@gmail.com>
2019-08-10 13:56:08 -0500

    Update bash script

--- git/migrate_github_repo_to_bitbucket.sh
+++ git/migrate_github_repo_to_bitbucket.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-GITHUB_USER=user1
-GITHUB_REPO_NAME=test1
+GITHUB_USER=bitclaw
+GITHUB_REPO_NAME=mongodb-docker
 
-BITBUCKET_USER=user2
-BITBUCKET_REPO_NAME=test2
+BITBUCKET_USER=bitclaw
+BITBUCKET_REPO_NAME=mongodb-docker
 
 git clone --mirror git@github.com:${GITHUB_USER}/${GITHUB_REPO_NAME}.git
+cd ${GITHUB_REPO_NAME}.git || exit
+#git remote -v
+#git remote rm origin
 git remote add bitbucket git@bitbucket.org:${BITBUCKET_USER}/${BITBUCKET_REPO_NAME}.git
 git push bitbucket --mirror
+rm -rf ${GITHUB_REPO_NAME}.git
