1a7a7c7093a1d15b1091e96921d32321b0eff083
Daniel Chavez <bitclaw@gmail.com>
2019-10-03 10:24:54 -0500

    Update bash script

--- git/most_used.sh
+++ git/most_used.sh
@@ -2,3 +2,6 @@
 
 git update-index --assume-unchanged src/main/resources/application.yml
 git update-index --assume-unchanged src/assets/config.json
+
+# Check all the commit hashes between two dates
+git log --after="2018-05-01" --before="2018-05-30" --oneline
