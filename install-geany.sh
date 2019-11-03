576701670d4c6f90f8adeccf7e00819c0d1602cf
Daniel C <bitclaw@gmail.com>
2019-11-02 20:01:26 -0500

    Update bash script

--- install-geany.sh
+++ install-geany.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt remove gedit -y
+sudo apt install geany -y
 git clone https://github.com/codebrainz/geany-themes.git ~/code/github/public/geany-themes
 cd ~/code/github/public/geany-themes
 bash install.sh
