64674ca2d4fdd7e507572a7fb7cb3dccf2a7c5fb
Daniel C <bitclaw@gmail.com>
2019-11-02 19:59:12 -0500

    Update bash script

--- ubuntu_18.04_post_install_setup.sh
+++ ubuntu_18.04_post_install_setup.sh
@@ -198,3 +198,11 @@ sudo update-alternatives --config editor
 # to set the default editor you can add the following to your shell configuration ( ~/.bashrc):
 # export VISUAL="/usr/bin/vim"
 # export EDITOR="$VISUAL"
+
+# SSH PERMISSION FIX WHEN COPYING FOLDER FROM ANOTHER COMPUTER
+chmod 700 ~/.ssh
+chmod 644 ~/.ssh/authorized_keys
+chmod 644 ~/.ssh/known_hosts
+chmod 644 ~/.ssh/config
+chmod 600 ~/.ssh/id_rsa
+chmod 644 ~/.ssh/id_rsa.pub
