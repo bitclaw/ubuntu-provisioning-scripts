0929e3936cfbad305906a3f4764c64b85343535f
Daniel Chavez <bitclaw@gmail.com>
2020-03-27 07:47:41 -0500

    Update bitbucket migrate script

--- git/migrate_bitbucket_repo_to_another_bitbucket.sh
+++ git/migrate_bitbucket_repo_to_another_bitbucket.sh
@@ -7,5 +7,6 @@ NEW_BITBUCKET_USER=user2
 NEW_REPO_NAME=test2
 
 git clone --mirror git@bitbucket.org:${BITBUCKET_USER}/${REPO_NAME}.git
+cd ${REPO_NAME}.git || exit
 git remote add bitbucket git@bitbucket.org:${NEW_BITBUCKET_USER}/${NEW_REPO_NAME}.git
 git push bitbucket --mirror
