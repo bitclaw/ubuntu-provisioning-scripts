041f94e3dd7807498b922dcca070f4a1f928388e
Daniel Chavez <bitclaw@gmail.com>
2020-08-16 04:43:05 -0500

    Add android script

--- android/access_android_devices_in_ubuntu.sh
+++ android/access_android_devices_in_ubuntu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # See https://www.debugpoint.com/2016/03/how-to-access-android-devices-internal-storage-and-sd-card-in-ubuntu-linux-mint-using-media-transfer-protocol-mtp/
+# See https://askubuntu.com/questions/1038145/trying-to-install-android-file-transfer-ubuntu-18-04-bionic
 
-sudo apt-get install go-mtpfs -y && sudo apt-get install libmtp -y && sudo apt-get install mtpfs mtp-tools -y
+sudo apt-get install go-mtpfs -y && sudo apt-get install libmtp9 -y && sudo apt-get install mtpfs mtp-tools -y
 
 
