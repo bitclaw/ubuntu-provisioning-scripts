5dbb9602649c18955a85e9b5e59c6427701572f8
Daniel Chavez <bitclaw@gmail.com>
2020-09-17 09:36:26 -0500

    Install helm in kubernetes install script

--- k8s/install.sh
+++ k8s/install.sh
@@ -4,7 +4,7 @@ curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s ht
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 kubectl version --client
-exit 0
+
 
 # Ubuntu, Debian or HypriotOS
 # sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
@@ -12,3 +12,11 @@ exit 0
 # echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 # sudo apt-get update
 # sudo apt-get install -y kubectl
+
+# Install Helm, see https://helm.sh/docs/intro/install/
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+helm version
+
+exit 0
