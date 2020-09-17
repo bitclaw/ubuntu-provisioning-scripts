9ffe6af708466d734cd3a43e323ac6e69e41b74e
Daniel Chavez <bitclaw@gmail.com>
2020-09-17 09:32:59 -0500

    Add kubernetes install script

---
+++ k8s/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+exit 0
+
+# Ubuntu, Debian or HypriotOS
+# sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
+# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+# sudo apt-get update
+# sudo apt-get install -y kubectl
