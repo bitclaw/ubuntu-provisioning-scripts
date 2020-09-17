688e798a480d6a570ecb34a5bd86cb1d971d026d
Daniel Chavez <bitclaw@gmail.com>
2020-09-17 09:41:15 -0500

    Install kops in kubernetes install script

--- k8s/install.sh
+++ k8s/install.sh
@@ -19,4 +19,10 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 helm version
 
+# Install kops, see https://kops.sigs.k8s.io/getting_started/install/
+curl -LO https://github.com/kubernetes/kops/releases/download/"$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)"/kops-linux-amd64
+chmod +x kops-linux-amd64
+sudo mv kops-linux-amd64 /usr/local/bin/kops
+kops version
+
 exit 0
